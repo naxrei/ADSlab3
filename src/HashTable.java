bc2a42724f0c4dacd76162df8ad8b0dca18ed6d3
naxrei <Naxreius1488@gmail.com>
2023-07-25 21:12:16 +0600

    Main code

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,2 +1,108 @@
-package PACKAGE_NAME;public class HashTable {
+import java.util.LinkedList;
+
+public class HashTable<K, V> {
+    private LinkedList<HashNode<K, V>>[] chain;
+    private int M = 11;
+    private int size;
+    public HashTable() {
+        chain = new LinkedList[M];
+        size = 0;
+    }
+    public HashTable(int m) {
+        this.M = m;
+        chain = new LinkedList[M];
+        size = 0;
+    }
+    private int hash(K key) {
+        return key.hashCode() % M;
+    }
+    public void put(K key, V value) {
+        int index = hash(key);
+        if (chain[index] == null){
+            chain[index] = new LinkedList<HashNode<K, V>>();
+        }
+        for (HashNode<K, V> node: chain[index]){
+            if(node.getKey().equals(key)){
+                node.setValue(value);
+                return;
+            }
+        }
+        chain[index].add(new HashNode<K, V>(key, value));
+        size++;
+    }
+    public V get(K key) {
+        int index = hash(key);
+        if (chain[index] == null) return null;
+
+        for (HashNode<K, V> node: chain[index]){
+            if(node.getKey().equals(key)) return node.getValue();
+        }
+        return null;
+    }
+    public V remove(K key) {
+        int index = hash(key);
+        if (chain[index] == null) return null;
+
+        for (HashNode<K, V> node: chain[index]){
+            if(node.getKey().equals(key)) {
+                chain[index].remove(node);
+                size--;
+                return node.getValue();
+            }
+        }
+        size--;
+        return null;
+    }
+    public boolean contains(V value) {
+        for(LinkedList<HashNode<K,V>> list: chain) {
+            for(HashNode<K,V> node: list){
+                if (node.getValue().equals(value)) return true;
+            }
+        }
+        return false;
+    }
+    public K getKey(V value) {
+        for(LinkedList<HashNode<K,V>> list: chain) {
+            for(HashNode<K,V> node: list){
+                if (node.getValue().equals(value)) return node.getKey();
+            }
+        }
+        return null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * replace
+     * @param key provides key that we need to calculate as hash
+     * @param oldValue value for replace
+     * @param newValue new value that replaces oldValue
+     * @return true if oldValue were replaced, else otherwise
+     * **/
+    public boolean replace(K key, V newValue, V oldValue) {
+        int index = hash(key);
+        LinkedList<HashNode<K, V>> bucket = chain[index];
+        if (bucket != null) {
+            for (HashNode<K, V> item: bucket) {
+                if(item.getValue().equals(oldValue)){
+                    item.setValue(newValue);
+                    return true;
+                }
+            }
+        }
+        return false;
+
+
+    }
+
+    @Override
+    public String toString() {
+        String out = "";
+        for(int i = 0; i<this.chain.length;i++) {
+            out += "Bucket " + i + ": " + chain[i].size() + "\n";
+        }
+        return out;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Random;
+
+class MyTestingClass{
+    private int val;
+    public MyTestingClass(int v) {
+        this.val = v;
+    }
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(val);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashTable hashTable = new HashTable<MyTestingClass, String>();
+        Random rand = new Random();
+//        for (int i = 0; i < 10000; i++) {
+//            int val = rand.nextInt(0, 9999999);
+//            hashTable.put(new MyTestingClass(val), "Student " + val);
+//        }
+//
+//        System.out.println(hashTable);
+
+        MyTestingClass key = new MyTestingClass(1337);
+        hashTable.put(key, "Test case");
+        hashTable.replace(key, "New Test case", "Test case");
+        System.out.println(hashTable.get(key));
     }
 }
